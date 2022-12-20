940230218323dec609bc17400a1a9838c35ab056
VespaLoick <118724077+VespaLoick@users.noreply.github.com>
2022-12-20 15:08:47 +0100

    add control map and player prefab

---
+++ porte/Assets/protagoniste/protagonist_control.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class protagonist_control : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public InputActionMap controlavatar;
+    void Start()
+    {
+        controlavatar.Enable();        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+   
+   
+}
