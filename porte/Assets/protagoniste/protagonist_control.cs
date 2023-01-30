3dfde06659cd96fb08891778646fcbf623703ae0
VespaLoick <118724077+VespaLoick@users.noreply.github.com>
2023-01-30 17:02:20 +0100

    implementation new imput system

--- porte/Assets/protagoniste/protagonist_control.cs
+++ porte/Assets/protagoniste/protagonist_control.cs
@@ -3,21 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class protagonist_control : MonoBehaviour
+public class protagonist_control : MonoBehaviour 
 {
     // Start is called before the first frame update
+    //private carte_de_control controleur ;
 
-    public InputActionMap controlavatar;
     void Start()
-    {
-        controlavatar.Enable();        
+    {        
+        Debug.Log("WELCOME!");    
     }
     // Update is called once per frame
     void Update()
     {
-        
+
     }
-    
-   
-   
+
+    private void OnTestaction(InputValue touche)
+    {
+        Debug.Log(touche);
+    }
+
+    public void testaction(InputAction.CallbackContext context)
+    {
+       Debug.Log("oooooooooo!");
+    }
+
 }
